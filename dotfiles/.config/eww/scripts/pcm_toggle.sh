bdeefc3b52303e33c86bbf89e30c61201956b2e4
isaac09809 <joseluis-jl-1@hotmail.com>
2024-05-23 15:23:49 -0600

    20240523

---
+++ dotfiles/.config/eww/scripts/pcm_toggle.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+pcm=$(pgrep picom)
+#echo $pcm
+if [[ $pcm ]]; then
+	cp /home/isaac09809/.config/i3/config_picom_off /home/isaac09809/.config/i3/config
+	i3-msg restart
+	eww close cattpuccin
+	eww open cattpuccin_no_pcm
+	pkill -9 picom
+	eww update is_pcm_active=0
+else
+	cp /home/isaac09809/.config/i3/config_picom_on /home/isaac09809/.config/i3/config
+	i3-msg restart
+	picom -b --config /home/isaac09809/.config/picom/picom.conf
+	eww close cattpuccin_no_pcm
+	eww open cattpuccin
+	eww update is_pcm_active=1
+fi
