c427cd1f53e9e30f1878f8426a328ad93ee898ab
isaac09809 <joseluis-jl-1@hotmail.com>
2024-05-28 20:38:59 -0600

    20240528

--- dotfiles/.config/eww/scripts/pcm_toggle.sh
+++ dotfiles/.config/eww/scripts/pcm_toggle.sh
@@ -3,7 +3,7 @@ pcm=$(pgrep picom)
 #echo $pcm
 if [[ $pcm ]]; then
 	pkill -9 picom
-    sed -i "248,255d" $HOME/.config/i3/config
+    sed -i "248,253d" $HOME/.config/i3/config
     sed -i "245d" $HOME/.config/i3/config
     sed -i "245i default_border pixel 5" $HOME/.config/i3/config
     sed -i "248d" $HOME/.config/i3/config
@@ -14,11 +14,11 @@ if [[ $pcm ]]; then
 	i3-msg restart
 else
 	picom -b --config /home/isaac09809/.config/picom/picom.conf
-    sed -i '247i \\ngaps top -15'\\n'gaps right 10'\\n'gaps bottom 10'\\n'gaps left 10'\\n'gaps inner 15'\\n\\n'smart_gaps off' $HOME/.config/i3/config
+    sed -i "248d" $HOME/.config/i3/config
+    sed -i "248i #smart_borders on" $HOME/.config/i3/config
+    sed -i '247i \\ngaps top -15'\\n'gaps right 10'\\n'gaps bottom 10'\\n'gaps left 10'\\n'gaps inner 15' $HOME/.config/i3/config
     sed -i "245d" $HOME/.config/i3/config
     sed -i "245i default_border pixel 0" $HOME/.config/i3/config
-    sed -i "254d" $HOME/.config/i3/config
-    sed -i "254i #smart_borders on" $HOME/.config/i3/config
 	eww close cattpuccin_X11_no_pcm
 	eww open cattpuccin_X11
 	eww update is_pcm_active=1
